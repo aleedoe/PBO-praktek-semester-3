2d8cc45f7bbcff8780b2498e7bd5640e0bdcf528
alie <mushyafaali@gmail.com>
2023-10-07 11:59:41 +0700

    membuat class admin ~week-2/main.py

---
+++ week-2/main.py
@@ -0,0 +1,8 @@
+class Admin() :
+    def __init__(self, id, nama, email):
+        self.id = id
+        self.nama = nama
+        self.email = email
+
+    def tambahProduk() :
+        print("tambah produk")
